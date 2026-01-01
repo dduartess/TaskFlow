2b621f619e5c837f4289e89c510474ba7cf63fab
dduartess <dandandsoares@gmail.com>
2026-01-01 19:26:20 -0300

    feat: create task controller and task validation on service for create future services features

---
+++ src/main/java/com/example/taskflow/service/taskValidation.java
@@ -0,0 +1,25 @@
+package com.example.taskflow.service;
+
+public class taskValidation {
+
+public boolean taskTitleIsValid(String title){
+    if (title == null || title.trim().isEmpty()) {
+        System.out.println("Invalid task title.");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+public boolean taskDescriptionIsValid(String description){
+    if (description == null || description.trim().isEmpty()) {
+        System.out.println("Invalid task description.");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+}
