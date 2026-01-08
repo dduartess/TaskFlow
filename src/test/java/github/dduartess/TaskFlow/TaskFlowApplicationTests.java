e7f64f4103a651ad88a197842ac1d61005e898d3
dduartess <dandandsoares@gmail.com>
2026-01-08 09:00:59 -0300

    Chore: Initial project structure

---
+++ src/test/java/github/dduartess/TaskFlow/TaskFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package github.dduartess.TaskFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
