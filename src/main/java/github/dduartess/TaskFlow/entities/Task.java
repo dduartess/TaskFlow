856f8f32dc38f388cc1941bc143dc02763f9e5d4
dduartess <dandandsoares@gmail.com>
2026-01-08 09:10:47 -0300

    Feat: Create Task entity and TaskStatus enum

---
+++ src/main/java/github/dduartess/TaskFlow/entities/Task.java
@@ -0,0 +1,91 @@
+package github.dduartess.TaskFlow.entities;
+
+import java.io.Serializable;
+
+import github.dduartess.TaskFlow.entities.enums.TaskStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, TaskStatus status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Task other = (Task) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+}
---
+++ src/main/java/github/dduartess/TaskFlow/entities/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package github.dduartess.TaskFlow.entities.enums;
+
+public enum TaskStatus {
+
+    COMPLETED,
+    PENDING
+}
