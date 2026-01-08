5420926675fca6150414e8b17a933bdd03473d4a
dduartess <dandandsoares@gmail.com>
2026-01-08 09:35:42 -0300

    Feat: Create methods: findAll(GetMapping) insert(PostMapping) in TaskController

--- src/main/java/github/dduartess/TaskFlow/controllers/TaskController.java
+++ src/main/java/github/dduartess/TaskFlow/controllers/TaskController.java
@@ -1,5 +1,38 @@
 package github.dduartess.TaskFlow.controllers;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import github.dduartess.TaskFlow.entities.Task;
+import github.dduartess.TaskFlow.services.TaskService;
+
+@RestController
+@RequestMapping(value = "/tasks")
 public class TaskController {
 
+    @Autowired
+    private TaskService service;
+
+    @GetMapping
+    public ResponseEntity<List<Task>> findAll(){
+        List<Task> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> insert(@RequestBody Task task){
+        task = service.save(task);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(task.getId()).toUri();
+        return ResponseEntity.created(uri).body(task);
+    }
 }
